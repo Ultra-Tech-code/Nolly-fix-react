c5e1713a288708310f45ca2a23a121e6b423a3bf
Ultra-tech-code <hizick27@gmail.com>
2022-07-31 21:27:36 +0100

    added a counter. the like button is functional now

--- src/component/Mainpagecontent.jsx
+++ src/component/Mainpagecontent.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import "../App.css"
+import { useState } from 'react'
 
 const Mainpagecontent = (props) => {
+  const[count, setcount] = useState(0)
+
+  let increment=()=>{
+    setcount(count + 1)
+    
+  };
   return (
         <div className="card">
             <img src={props.img} alt="The redemption" style={{width:"100%"}} />
@@ -9,7 +16,7 @@ const Mainpagecontent = (props) => {
             <h1>{props.movieName}</h1>
             <p className="price">#2,000</p>
             <p>{props.movieAttr}</p>
-            <p><button className="btn btn-lg">like <i className="fas fa-heart"></i> <span className="counter" />0</button></p>
+            <p><button onClick={increment} className="btn btn-lg"> like <i className="fas fa-heart"></i> {count}</button></p>
           </div> 
   )
 }
